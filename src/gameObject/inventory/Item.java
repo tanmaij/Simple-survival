fe44ad89d3545a026450318823ff8492eb64967a
tanmaij <killdogvinam@gmail.com>
2022-03-19 10:47:51 +0700

    adding inventory

---
+++ src/gameObject/inventory/Item.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gameObject.inventory;
+
+import java.awt.image.BufferedImage;
+
+/**
+ *
+ * @author HP
+ */
+public class Item {
+    BufferedImage image;
+    String name;
+}
